fb354dff5254630feede135047c141c58903584e
Leo Xie <leoxie@LeodeMacBook-Air-9.local>
2025-10-16 14:10:56 -0400

    submit

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -8,7 +8,28 @@ LinkedList::~LinkedList() {
 }
 void LinkedList::push_front(int v) { head_ = new Node(v, head_); }
 void LinkedList::remove_evens() {
-    // TODO: implement
+    while (head_ != nullptr && head_->value % 2 == 0) {
+        Node* old_head = head_;
+        head_ = head_->next;
+        delete old_head;
+    }
+
+  
+    Node* prev = head_;
+    Node* current = (head_ != nullptr) ? head_->next : nullptr;
+
+    while (current != nullptr) {
+        if (current->value % 2 == 0) {
+            prev->next = current->next;
+            Node* node_to_delete = current;
+            current = current->next;
+            delete node_to_delete;
+
+        } else {
+            prev = current;
+            current = current->next;
+        }
+    }
 }
 std::string LinkedList::to_string() const {
     std::ostringstream oss;
